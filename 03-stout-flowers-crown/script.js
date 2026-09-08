095de9dcaef23f00233674873b4933fd5897b04d
Gaelle N. <65255801+gaelen17@users.noreply.github.com>
2026-09-08 18:30:42 +0200

    Create script.js

---
+++ 03-stout-flowers-crown/script.js
@@ -0,0 +1,40 @@
+import gsap from "https://cdn.skypack.dev/gsap";
+
+const flowers = document.querySelectorAll(".flower-inner");
+const petals = document.querySelectorAll(".petals path");
+
+gsap.fromTo(flowers, 
+  { opacity: 0 }, 
+  { opacity: 1, duration: 3, ease: "power1.inOut" }
+);
+
+flowers.forEach((flower, index) => {
+  gsap.to(flower, {
+    rotation: index % 2 === 0 ? 8 : -8,
+    y: "+=4",
+    duration: 2.5 + gsap.utils.random(-0.3, 0.3),
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut",
+    delay: index * 0.2
+  });
+});
+
+petals.forEach((petal) => {
+  gsap.to(petal, {
+    opacity: gsap.utils.random(0.7, 1),
+    duration: gsap.utils.random(0.8, 1.5),
+    repeat: -1,
+    yoyo: true,
+    ease: "sine.inOut",
+    delay: gsap.utils.random(0, 2)
+  });
+});
+
+gsap.to(".crown-group", {
+  rotation: 360,
+  duration: 100,
+  repeat: -1,
+  ease: "none",
+  transformOrigin: "50% 50%"
+});
